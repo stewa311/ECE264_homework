27acd4a183f1c9024a3f90e4094d43b669cdbc9a
Matthew James Stewart <stewa311@ecegrid-thin7.ecn.purdue.edu>
2020-12-01 12:56:43 -0500

    final submission

--- HW15BinaryTree1/tree.c
+++ HW15BinaryTree1/tree.c
@@ -61,7 +61,55 @@ void preOrder(Tree * tr, char * filename)
 // Consider the algorithm posted on
 // https://www.geeksforgeeks.org/construct-a-binary-tree-from-postorder-and-inorder/
 
+TreeNode* newtNode(int value)
+{
+  TreeNode* tnode = (TreeNode*)malloc(sizeof(TreeNode));
+  tnode->value = value;
+  tnode->left = tnode->right = NULL;
+  return (tnode);
+}
+
+int search(int arr[], int strt, int end, int value)
+{
+  int i;
+  for (i = strt; i <= end; i++) 
+  {
+    if (arr[i] == value)
+    {
+      break;
+    }
+  }
+  return i;
+}
+
+TreeNode* buildtnode(int in[], int post[], int inStrt, int inEnd, int* pIndex)
+{
+  if (inStrt > inEnd)
+  {
+    return NULL;
+  }
+  
+  TreeNode* tnode = newtNode(post[*pIndex]);
+  (*pIndex)--;
+  
+  if (inStrt == inEnd)
+  {
+    return tnode;
+  }
+  
+  int iIndex = search(in, inStrt, inEnd, tnode->value);
+  
+  tnode->right = buildtnode(in, post, iIndex + 1, inEnd, pIndex);
+  tnode->left = buildtnode(in, post, inStrt, iIndex - 1, pIndex);
+  
+  return tnode;
+}
+
 Tree * buildTree(int * inArray, int * postArray, int size)
 {
+  int pIndex = size - 1;
+  Tree * trnode = (Tree*)malloc(sizeof(Tree));
+  trnode->root = buildtnode(inArray, postArray, 0, size - 1, &pIndex);
+  return trnode;
 }
 #endif
